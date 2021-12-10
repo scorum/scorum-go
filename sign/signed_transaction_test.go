a3fa21cc4ae97a6650cdf4d96f0a919a504dc593
Konstantin Shafransky <kostas.shafransky@scorum.com>
2021-12-10 17:25:35 +0300

    proper test on real rx from testnet

--- sign/signed_transaction_test.go
+++ sign/signed_transaction_test.go
@@ -2,10 +2,11 @@ package sign
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"testing"
 	"time"
 
-	"github.com/btcsuite/btcutil"
+	"github.com/btcsuite/btcutil/base58"
 	"github.com/scorum/scorum-go/types"
 	"github.com/stretchr/testify/require"
 )
@@ -38,38 +39,38 @@ func TestTransaction_Digest(t *testing.T) {
 }
 
 func TestTransaction_Verify(t *testing.T) {
-	var tx *types.Transaction
-	// Prepare the transaction.
-	expiration := time.Date(2016, 8, 8, 12, 24, 17, 0, time.UTC)
-	tx = &types.Transaction{
-		RefBlockNum:    36029,
-		RefBlockPrefix: 1164960351,
-		Expiration:     &types.Time{&expiration},
-	}
-	tx.PushOperation(&types.VoteOperation{
-		Voter:    "xeroc",
-		Author:   "xeroc",
-		Permlink: "piston",
-		Weight:   10000,
-	})
+	txStr := `
+    {
+        "ref_block_num": 57753,
+        "ref_block_prefix": 1882698764,
+        "expiration": "2021-12-01T17:26:00",
+        "operations": [
+            [
+                "transfer",
+                {
+                    "from": "azucena",
+                    "to": "leonarda",
+                    "amount": "0.000009000 SCR",
+                    "memo": "{\"bet_id\":\"8b022219-3825-413e-a6ae-1cc3154bdb7f\",\"game_id\":\"17ff54ad-8472-4e4f-9e96-2b6ccbfaef33\"}"
+                }
+            ]
+        ],
+        "extensions": [],
+        "signatures": [
+            "204da1d0e0c5cb39978d1ad4fe4cd4446718ad0e10a19d662ac7bc9e0bba708f2c2116279fdc512d03fe2c97cdfc00804a4b958a8078f9b3b28e4f4b22f798f032"
+        ],
+        "transaction_id": "825d2733272648f95c21ae97be9f8c3d8edaf8f9",
+        "block_num": 31449514,
+        "transaction_num": 0
+    }`
 
-	var initChain = &Chain{
-		ID: "0000000000000000000000000000000000000000000000000000000000000000",
-	}
+	var tx types.Transaction
+	err := json.Unmarshal([]byte(txStr), &tx)
+	stx := NewSignedTransaction(&tx)
 
-	expectedDigest := "582176b1daf89984bc8b4fdcb24ff1433d1eb114a8c4bf20fb22ad580d035889"
-	stx := NewSignedTransaction(tx)
-	digest, err := stx.Digest(initChain)
-	require.NoError(t, err)
-	require.Equal(t, expectedDigest, hex.EncodeToString(digest))
+	keyWithChecksum := base58.Decode("7cTf2Dx9rxffs6E2z2pdn5cLMneo3AAFSsF9g4SaVviCYdfQ63")
 
-	wif := "5HzA7Eju6BQcunCHNy5ywGG2YriZMQaEcoPEbQFpSRk9NZEo6Fv"
-	require.NoError(t, stx.Sign([]string{wif}, initChain))
-
-	btcecWif, err := btcutil.DecodeWIF(wif)
-	require.NoError(t, err)
-
-	res, err := stx.Verify(initChain, [][]byte{btcecWif.PrivKey.PubKey().SerializeUncompressed()})
+	res, err := stx.Verify(TestChain, [][]byte{keyWithChecksum[:len(keyWithChecksum)-4]})
 	require.NoError(t, err)
 	require.True(t, res)
 }
